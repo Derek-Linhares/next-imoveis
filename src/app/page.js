c0ae8d4e5e999e149e51f6752e6ffccb1fab4af6
Derek-Linhares <derikbruno3@gmail.com>
2025-04-07 14:25:49 -0300

    add Header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-red-500">page</div>;
+  return <div className="text-red-500">pagesss</div>;
 };
 
 export default page;
